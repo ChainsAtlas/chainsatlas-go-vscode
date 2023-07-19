ff74e407e6cb61e222a6f01429e1be0eaca67762
Marcos Medeiros <marcos.medeiros@netlume.io>
2023-07-19 17:34:43 -0300

    Setup WalletConnect

---
+++ src/extension.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import fs from "fs";
+import path from "path";
+import vscode from "vscode";
+
+const activate = (context: vscode.ExtensionContext) => {
+  let disposable = vscode.commands.registerCommand(
+    "chainsatlas-go.helloWorld",
+    () => {
+      const panel = vscode.window.createWebviewPanel(
+        "catCoding",
+        "Cat Coding",
+        vscode.ViewColumn.One,
+        {},
+      );
+
+      panel.webview.html = getWebviewContent(context);
+    },
+  );
+
+  context.subscriptions.push(disposable);
+};
+
+const getWebviewContent = (context: vscode.ExtensionContext) => {
+  const templatePath = path.join(context.extensionPath, "src", "webview.html");
+
+  return fs.readFileSync(templatePath, "utf8");
+};
+
+const deactivate = () => {};
+
+export { activate, deactivate };
