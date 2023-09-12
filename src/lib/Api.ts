db4dd52bca6cdbcd8fcf4f46a86cd918debb8b09
Marcos Medeiros <marcos.medeiros@netlume.io>
2023-09-12 16:45:21 -0300

    chore: remove console.log

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -122,7 +122,6 @@ export class Api {
    * to provide user feedback and help improve ChainsAtlas GO.
    */
   public async sendTelemetry(body: string): Promise<void> {
-    console.log("TELEMETRY: ", body);
     await this._fetch(`${Api._URL}/telemetry`, {
       method: "POST",
       body,
